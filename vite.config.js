83e033b51fe933769bf06815bd5449bb069586b0
Yasin <bestplayer0202@gmail.com>
2026-07-08 02:13:23 +0500

    fix: chatbot

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,13 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: '127.0.0.1',
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
   },
   build: {
     rollupOptions: {
